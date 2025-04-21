71c7070dbef3f7cfe2502b38d4b6ff7a8f3b1297
k230061 Abdul.Rahman.Azam <k230061@khifast.nu.edu.pk>
2025-04-21 11:56:51 +0500

    added layout and calender

---
+++ src/components/common/calender.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Calendar as CalendarIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+
+
